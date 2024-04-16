94f3cca3f19a31d45869e953a00d4f37e3112110
asifreza-zhcet <asifreza.zhcet@gmail.com>
2024-04-16 09:47:09 +0530

    Update README.md

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'Location': 'Bengaluru, India',
+        'Salary': 'Rs. 10,00,000'
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'Location': 'Delhi, India',
+        'Salary': 'Rs. 15,00,000'
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Developer',
+        'Location': 'Remote',
+        'Salary': ''
+    },
+    {
+        'id': 4,
+        'title': 'Backend Developer',
+        'Location': 'San Francisco, USA',
+        'Salary': '$ 120,000'
+    }
+
+]
 
 
 @app.route('/')
 def hello_jovian():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS, company_name='Jovian')
 
 
-app.run(host='0.0.0.0', debug=True)
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
+
+
+app.run(debug=True)
